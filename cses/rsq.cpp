a6632b59b478e020887adbbe32366456a76c84ec
James Rayman <jamsramen@gmail.com>
2020-03-18 23:00:15 -0500

    cses golf

---
+++ cses/rsq.cpp
@@ -0,0 +1,9 @@
+#import <iostream>
+#define I std::cin >>
+ 
+long n, q, i, a, v ['   '];
+ 
+main () {
+    for (I n >> q; n--; v[++i] = v[i] + a) I a;
+    for (; q--; printf("%ld ", v[n] - v[i-1])) I i >> n;
+}
---
+++ cses/rxq.cpp
@@ -0,0 +1,9 @@
+#import <iostream>
+#define I std::cin >>
+ 
+int n, q, i, a, v ['   '];
+ 
+main () {
+    for (I n >> q; n--; v[++i] = v[i] ^ a) I a;
+    for (; q--; printf("%d ", v[n] ^ v[i-1])) I i >> n;
+}
